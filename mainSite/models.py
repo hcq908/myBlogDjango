56f1ebd59ee9d0a699534083c902a34fd84bf8ba
hcq908 <hcq908@163.com>
2018-08-05 06:30:40 -0700

    Using format function"
    git remote add origin https://github.com/hcq908/myBlogDjango.git
    
    exit
    exit
    quti
    quit

--- mainSite/models.py
+++ mainSite/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.CharField(max_length=200)
+    body = models.TextField()
+    pub_date = models.DateTimeField(default=timezone.now)
+
+    #定义排序方式
+    class Meta:
+        ordering = ('-pub_date',)
+    #定义返回缩略短语类型
+    def __unicode__(self):
+        return self.title
+
--- mainSite/views.py
+++ mainSite/views.py
@@ -1,3 +1,20 @@
+from django.template.loader import get_template
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+from datetime import datetime
 
 # Create your views here.
+def homepage(request):
+    template = get_template('index.html')
+    posts = Post.objects.all()
+    now = datetime.now()
+    html = template.render(locals())
+    return HttpResponse(html)
+
+    # post_lists = list() #建立空表
+    # for count, post in enumerate(posts):
+    #     post_lists.append("No.{}:".format(str(count))+str(post.title)+"<br>")
+    #     # <small>标签,在html中定义小型文本
+    #     post_lists.append("<small>" + str(post.body) + "</small><br><br>")
+    # return HttpResponse(post_lists)
